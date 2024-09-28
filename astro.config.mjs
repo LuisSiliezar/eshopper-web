013fb4e59acc8dec454c921cc5eb1a2c5e15df85
Luis Siliezar <lsiliezarjr@gmail.com>
2024-09-28 09:03:02 -0600

    initial commit

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,5 +1,19 @@
 // @ts-check
 import { defineConfig } from 'astro/config';
 
+import icon from 'astro-icon';
+
+import vercel from '@astrojs/vercel/serverless';
+
+import react from '@astrojs/react';
+
+import tailwind from '@astrojs/tailwind';
+
 // https://astro.build/config
-export default defineConfig({});
+export default defineConfig({
+  integrations: [icon(), react(), tailwind({
+    applyBaseStyles: false,
+  }),],
+  output: 'hybrid',
+  adapter: vercel()
+});
